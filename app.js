159c90efbf8904d7e5155bf48bc53d038f0a620f
rasiksilwal013 <rasiksilwal@gmail.com>
2023-11-23 00:06:26 -0500

    invoked honk method for each class

--- app.js
+++ app.js
@@ -33,6 +33,19 @@ class Car{
 
     }
 
+
     // creating the instances of each car models 
-    console.log (`Car 1 Details ${car1.honk()}`);
+
+    // instances of car class
+
+const car1 = new Car('Honda', 'CR-V', 2023, 'Red', 50000, 45);
+const car2 = new Car('Ford', 'F-150', 2020, 'Black', 25000, 30);
+const car3 = new Car('BMW', 'X5', 2022, 'Green', 60000, 65);
+const car4 = new Car('Mazda', 'CX-5', 2019, 'White', 15000, 60);
+const car5 = new Car('Audi', 'Q7', 2018, 'Silver', 52000, 47);
+const car6 = new Car('Kia', 'Forte', 2020, 'Blue', 21000, 56);
+
+// invoking honk method
+console.log (`Car 1 Details ${car1.honk()}`);
+   
 
