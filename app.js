85bb19b162dbc1aec70538e629837f68c3272ef3
rasiksilwal013 <rasiksilwal@gmail.com>
2023-11-22 23:45:26 -0500

    car class created including all the properties

---
+++ app.js
@@ -0,0 +1,12 @@
+// car class
+class Car{
+    constructor(brand, model, year, color, price, gas) { // constructor for class to add differenr cars
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+        this.price = price;
+        this.gas = gas;
+      }
+      
+    }
